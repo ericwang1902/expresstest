3d7cc11c034d62b808c865d01d875021f0e0d24b
ericwang1902 <2248930513@qq.com>
2016-03-20 20:09:01 +0800

    'test'

--- routes/wechats.js
+++ routes/wechats.js
@@ -6,7 +6,6 @@ var config={
   appid: 'wx45eb07597f1e004a',
   encodingAESKey: 'GwpmVSoQTqy55MMPA4qKyTTiQkFs8sPiSt5ou3Z0lSI'
 };
-module.exports = wechat(config, wechat.text(function(message,req,res,next){
+module.exports = wechat(config).text(function(message,req,res,next){
   console.log(message)
-})
-);
+}).middlewarify();
