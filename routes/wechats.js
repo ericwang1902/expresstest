fcc1c89f007bd29f7e33c9922082e9f2bbd44b39
ericwang1902 <2248930513@qq.com>
2016-03-20 20:02:06 +0800

    'test'

--- routes/wechats.js
+++ routes/wechats.js
@@ -6,9 +6,8 @@ var config={
   appid: 'wx45eb07597f1e004a',
   encodingAESKey: 'GwpmVSoQTqy55MMPA4qKyTTiQkFs8sPiSt5ou3Z0lSI'
 };
-router.get('/', wechat(config, function (req, res, next) {
-  // 微信输入信息都在req.weixin上
-  var message = req.weixin;
-  res.reply('Hello world!');
-}));
+router.get('/', wechat(config, wechat.text(function(message,req,res,next){
+  console.log(message)
+})
+));
 module.exports = router;
