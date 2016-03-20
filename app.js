1162bceb94e4ba7d1c97ca542ec304bf456a9f46
ericwang1902 <2248930513@qq.com>
2016-03-20 18:57:05 +0800

    'test'

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
+
 var wechats = require('./routes/wechats');
 
 var app = express();
@@ -27,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/login',login);
-app.use('/wechat',wechats)
+app.use('/wechat',wechats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
