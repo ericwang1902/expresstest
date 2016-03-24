ac82b2608d2f6b89a5e56d57e0a2198a8b3cc637
ericwang1902 <2248930513@qq.com>
2016-03-24 18:48:52 +0800

    'test'

--- routes/apis/login.js
+++ routes/apis/login.js
@@ -10,7 +10,7 @@ router.post('/', function(req, res, next) {
   console.info(req.body.user);
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "X-Requested-With")
-  res.writeHead(200,{'Content-Type': 'text/plain'});
+  //res.writeHead(200,{'Content-Type': 'text/plain'});
   res.write(JSON.stringify(data));
   res.end();
 });
